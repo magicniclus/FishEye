9d626395b3b6301de5f32e2c7dc6bae3f55fb809
Nicolas <casteranicolas.contact@gmail.com>
2021-09-27 16:17:11 +0200

    Création de le constante main sur le body de la page index.html; Appel de la class "IndexPage" , appel de de la data et ajout du domTarget dans la main

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,9 @@
 //Gestion des données de la page index
+
+//Body Index
+
+const main = document.createElement('main');
+const indexBody = document.querySelector(".bodyIndex");
+indexBody.appendChild(main);
+
+const indexPage = new IndexPage('https://magicniclus.github.io/FishEye/Js/Data/data.json', main)
