1cf3866eafd4a6de5b40e72f4c92f439ffe2e538
Nicolas <casteranicolas.contact@gmail.com>
2021-09-27 16:33:31 +0200

    Création de la class MediaProfil
    Elle à pour objectif des gerer l'&affichage des différents types de media en fonction de la demande de façon automatique.

--- Js/Components/mediaProfil.js
+++ Js/Components/mediaProfil.js
@@ -1,1 +1,52 @@
 //Ajout des media en fonction de leurs type
+
+class MediaProfil{
+    constructor(data, domTarget){
+        this.DOM = document.createElement("article");
+        this.DOM.setAttribute("class", 'photographerImg')
+        domTarget.appendChild(this.DOM);
+        for( const [key, value] of Object.entries(data)){
+            this[key] = value;
+        }
+        
+        this.render();
+    }
+
+    render(){
+        this.DOM.innerHTML = this.image ? this.templateImage() : this.templateVideo();
+
+
+        const likesImg = document.querySelector('.likesImg');
+    }
+
+
+    templateImage(){
+        return `
+                <img class="photoImg" src="Sample_Photos/${this.image}" alt="${this.title}">    
+                <div class="bottomImg">
+                    <span class="titleImg">${this.title}</span>
+                    <span class="priceImg">${this.price}€</span>
+                    <div class="likeImgGlobal">
+                        <span class="likesImg">${this.likes}</span>
+                        <i class="fas fa-heart"></i>
+                    </div>    
+                </div>   
+        `;
+    }
+
+    templateVideo(){
+        return `
+                <video autoplay loop> 
+                    <source src="Sample_Photos/${this.video}" type=video/mp4 alt="${this.title}">
+                </video>   
+                <div class="bottomVideo">
+                    <span class="titleVideo">${this.title}</span>
+                    <span class="priceVideo">${this.price}€</span>
+                    <div class="likeVideoGlobal">
+                        <span class="likesVideo">${this.likes}</span>
+                        <i class="fas fa-heart"></i>
+                    </div>    
+                </div>   
+        `;
+    }
+}
