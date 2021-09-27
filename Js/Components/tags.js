43a78528c92ee1242b009b2901ab9cc0d658fb7f
Nicolas <casteranicolas.contact@gmail.com>
2021-09-27 16:25:42 +0200

    Création de la class Tags

--- Js/Components/tags.js
+++ Js/Components/tags.js
@@ -1,1 +1,21 @@
 // Création des tags pour le header
+
+class Tags {
+    /**
+     * [constructor description]
+     *
+     * @param   {String}  props      nom du tag
+     * @param   {HTMLElement}  domTarget  [domTarget description]
+     * @param   {Function}  callback
+     */
+    constructor (props, domTarget, callback) {
+        this.DOM = document.createElement('button');
+        this.DOM.setAttribute('class', 'tag');
+        domTarget.appendChild(this.DOM);
+
+        this.DOM.innerHTML = `#${props}`;
+        this.DOM.onclick= function(){
+            callback(props);
+        };
+    }
+}
