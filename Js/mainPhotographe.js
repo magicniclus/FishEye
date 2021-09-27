e9e71b6a1dff6444d6872c317038093eb52c8d91
Nicolas <casteranicolas.contact@gmail.com>
2021-09-27 16:33:59 +0200

    Gestion de la page photographerPage.html en js

--- Js/mainPhotographe.js
+++ Js/mainPhotographe.js
@@ -1,1 +1,8 @@
 // Gestion des données de la page photographe
+
+const mainPhotographeProfil = document.createElement('main')
+const photographerPageBody = document.querySelector('.photographerPageIndex')
+photographerPageBody.appendChild(mainPhotographeProfil);
+
+
+const photographerPage = new PhotographerPage('https://magicniclus.github.io/NicolasCastera_6_06052021/data/data.json', mainPhotographeProfil)
