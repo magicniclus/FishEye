c5d2acd7da22e066a69361e4b7eefee036dfb754
Nicolas <casteranicolas.contact@gmail.com>
2021-09-27 16:22:34 +0200

    Création de la class PhotographerPage
    Création de la class PhotographerPage pour la gestion de l'affichage de la page photographerPage.html avec appel des class profilPhotographe et MediaProfil

--- Js/Pages/photographerPage.js
+++ Js/Pages/photographerPage.js
@@ -1,1 +1,34 @@
 // Gestion de l'affichage de la page photographe
+
+class PhotographerPage {
+
+    constructor (props, domTarget) {
+        this.dataManager = new DataManager(props);
+        this.DOM = domTarget;
+        this.id = parseInt(window.location.search.slice(4));
+        this.render ();
+    }
+
+
+    async render () {
+
+        this.showProfilPhotographe();
+        this.showMediaProfil();
+    }
+
+
+    async showProfilPhotographe () {
+        const data = await this.dataManager.getPhotographerById(this.id);
+        const newProfil = new ProfilPhotographe (data, this.DOM);
+    }
+
+
+    async showMediaProfil (){
+        const dataMedia = await this.dataManager.getMediaById(this.id);
+
+        dataMedia.forEach(media => {
+            new MediaProfil(media, this.DOM);
+        });
+    }
+
+}
